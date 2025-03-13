eac5b2cdb4f8e9bf187c003ccc90fc127c7449eb
fhao-3 <fabiohenriq.ao@gmail.com>
2025-03-13 15:34:52 -0300

    calculadora cria com sucesso

---
+++ calculadora_v2.py
@@ -0,0 +1,60 @@
+def adicao(num1, num2):
+    return num1 + num2
+
+
+def subtracao(num1, num2):
+    return num1 - num2
+
+
+def multiplicacao(num1, num2):
+    return num1 * num2
+
+
+def divisao(num1, num2):
+    try:
+        return num1 / num2
+    except ZeroDivisionError:
+        print('O divisor não pode ser igual a 0')
+
+
+def calculadora(num1, num2, operacao):
+    if operacao == '-' or operacao == 'subtração':
+        return subtracao(
+            num1=num1,
+            num2=num2,
+        )
+    elif operacao == '+' or operacao == 'adição':
+        return adicao(
+            num1=num1,
+            num2=num2,
+        )
+    elif operacao == '*' or operacao == 'multiplcação':
+        return multiplicacao(
+            num1=num1,
+            num2=num2,
+        )
+    elif operacao == '/' or operacao == 'divisão':
+        return divisao(
+            num1=num1,
+            num2=num2,
+        )
+
+
+saida = ''
+
+while saida != 'n':
+    n1 = int(input('Digite um número: '))
+    n2 = int(input('Digite outro número: '))
+    opcao = input('Qual operação deseja fazer? ')
+    resultado = calculadora(
+        num1=n1,
+        num2=n2,
+        operacao=opcao
+    )
+
+    print(f'Resultado da operação: {resultado}')
+
+    while True:
+        saida = input('Deseja continuar digite [S/N]: ').lower()
+        if saida == 'n'or saida == 's':
+            break
